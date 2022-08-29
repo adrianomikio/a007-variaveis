cd2b4d7b2c7379cbdcc21c76d4b216d5f1d79580
adrianomikio <adrianouge@gmail.com>
2022-08-29 15:25:16 -0300

    Primeira parte do exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const perguntaVestindoCamiseta = "Você está vestindo camiseta?"
+const perguntaVestindoRoupaPreta = "Você está vestindo roupa preta?"
+const perguntaSeSenteConfortávelHoje = "Você se sente confortável hoje?"
+
+let respostaVestindoCamiseta = "Sim"
+let respostaVestindoRoupaPreta = "Sim"
+let respostaSeSenteConfortávelHoje = "Sim"
+
+console.log(respostaVestindoCamiseta)
+console.log(respostaVestindoRoupaPreta)
+console.log(respostaSeSenteConfortávelHoje)
