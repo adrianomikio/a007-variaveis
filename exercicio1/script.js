e8d0e5892f1e6cc50481b7f6ce54f8430709a0aa
adrianomikio <adrianouge@gmail.com>
2022-08-29 14:45:27 -0300

    Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+// const nome = "Nome"
+// const idade = "21"
+
+// console.log (typeof nome, typeof idade)
+
+// Apareceu string string quando impresso porque ambas variáveis que estabeleci foram entre aspas (Strings)
+
+
+
+// const nome = prompt("Qual é seu nome?")
+// const idade = prompt("Qual é a sua idade?")
+
+// console.log (typeof nome, typeof idade)
+
+// Apareceu novamente String por eu não definir que tipo de variável será
+
+
+
+const nome = prompt("Qual é seu nome?")
+const idade = prompt("Qual é a sua idade?")
+
+console.log ("Olá", nome, ", você tem " ,idade, " anos")
